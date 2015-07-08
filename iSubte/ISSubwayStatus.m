21a903ec657a1fb6baf4b1cb51f51e0a2ea6ea74
Fernando Sproviero <fernandospr@gmail.com>
2015-07-08 13:10:05 -0300

    Adding model.

---
+++ iSubte/ISSubwayStatus.m
@@ -0,0 +1,13 @@
+//
+//  ISSubwayStatus.m
+//  iSubte
+//
+//  Created by Fernando Sproviero on 7/7/15.
+//  Copyright (c) 2015 FS. All rights reserved.
+//
+
+#import "ISSubwayStatus.h"
+
+@implementation ISSubwayStatus
+
+@end
