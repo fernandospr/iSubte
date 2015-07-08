3ece26725e21b57c433aeb0f0160a2047dfd9c9c
Fernando Sproviero <fernandospr@gmail.com>
2015-07-08 15:02:54 -0300

    Adding assembly.

---
+++ iSubte/ISApplicationAssembly.h
@@ -0,0 +1,13 @@
+//
+//  ISApplicationAssembly.h
+//  iSubte
+//
+//  Created by Fernando Sproviero on 7/8/15.
+//  Copyright (c) 2015 FS. All rights reserved.
+//
+
+#import <Typhoon/Typhoon.h>
+
+@interface ISApplicationAssembly : TyphoonAssembly
+
+@end
---
+++ iSubte/ISApplicationAssembly.m
@@ -0,0 +1,13 @@
+//
+//  ISApplicationAssembly.m
+//  iSubte
+//
+//  Created by Fernando Sproviero on 7/8/15.
+//  Copyright (c) 2015 FS. All rights reserved.
+//
+
+#import "ISApplicationAssembly.h"
+
+@implementation ISApplicationAssembly
+
+@end
