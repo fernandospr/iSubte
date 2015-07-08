0c52168ab7ff80f09e4497291f7b291d086b77c1
Fernando Sproviero <fernandospr@gmail.com>
2015-07-08 13:23:31 -0300

    Calling service to show the data.

--- iSubte/ISStatusTableViewController.m
+++ iSubte/ISStatusTableViewController.m
@@ -7,7 +7,55 @@
 //
 
 #import "ISStatusTableViewController.h"
+#import "ISSubwayStatus.h"
+#import "ISSubwayStatusClient.h"
+#import "ISSubwayStatusCell.h"
+
+@interface ISStatusTableViewController ()
+
+@property (strong, nonatomic) NSArray *items;
+
+@end
 
 @implementation ISStatusTableViewController
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    [self fetchItems];
+}
+
+- (void)fetchItems
+{
+    ISSubwayStatusClient *statusClient = [[ISSubwayStatusClient alloc] init];
+    __weak typeof(self) weakSelf = self;
+    [statusClient subwayStatusesWithSuccesBlockWithSuccessBlock:^(NSArray *subways) {
+        __strong typeof(self) strongSelf = weakSelf;
+        strongSelf.items = subways;
+        [strongSelf.tableView reloadData];
+    } errorBlock:^(NSError *error) {
+        NSLog(@"Error: %@", error);
+    }];
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.items.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    ISSubwayStatusCell *cell = [tableView dequeueReusableCellWithIdentifier:@"StatusCell" forIndexPath:indexPath];
+    
+    ISSubwayStatus *subway = self.items[indexPath.row];
+    [cell setNameLabelText:subway.name];
+    [cell setStatusLabelText:subway.status];
+    [cell setLineLabelText:[@"Línea " stringByAppendingString:subway.name]];
+    
+    return cell;
+}
 
 @end
