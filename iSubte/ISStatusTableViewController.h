ef79fda9af9fa195ce23317b008bd10da3617c43
Fernando Sproviero <fernandospr@gmail.com>
2015-07-08 13:04:46 -0300

    Adding view controller.

---
+++ iSubte/ISStatusTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ISStatusTableViewController.h
+//  iSubte
+//
+//  Created by Fernando Sproviero on 7/7/15.
+//  Copyright (c) 2015 FS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ISStatusTableViewController : UITableViewController
+
+@end
---
+++ iSubte/ISStatusTableViewController.m
@@ -0,0 +1,13 @@
+//
+//  ISStatusTableViewController.m
+//  iSubte
+//
+//  Created by Fernando Sproviero on 7/7/15.
+//  Copyright (c) 2015 FS. All rights reserved.
+//
+
+#import "ISStatusTableViewController.h"
+
+@implementation ISStatusTableViewController
+
+@end
